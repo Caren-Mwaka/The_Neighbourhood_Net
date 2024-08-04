bbd478394b4dff092595fb98a50007cf0f72fa48
Tatiana Mathia <mathiatatiana@gmail.com>
2024-08-05 01:27:04 +0300

    landing page

---
+++ client/src/LandingPage.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './LandingPage.css';
+import logoImage from './assets/images/logo.jpeg'; // Correct path to your logo
+import backgroundImage from './assets/images/landingpage.jpg'; // Correct path to your background image
+import shareImage from './assets/images/share.jpeg'; // Correct path to your share image
+import protectImage from './assets/images/protect.jpeg'; // Correct path to your protect image
+import engageImage from './assets/images/engage.jpeg'; // Correct path to your engage image
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressBook } from '@fortawesome/free-solid-svg-icons';
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className="section landing-section" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <header className="app-bar">
+          <div className="app-bar-links">
+            <a href="#contact">
+              <FontAwesomeIcon icon={faAddressBook} size="2x" />
+              CONTACT US
+            </a>
+            <a href="#about">ABOUT</a>
+          </div>
+          <div className="right-links">
+            <a href="#login">LOGIN</a>
+
+            
+            <Link to="/register">REGISTER</Link>
+          </div>
+        </header>
+        <img src={logoImage} alt="Logo" className="logo" />
+      </div>
+      <div className="section welcome-section">
+        <h1>Welcome to The Neighborhood Net</h1>
+        <p>Your Local Community Watch and Engagement Platform</p>
+        <div className="images-container">
+          <div className="image-card share">
+            <img src={shareImage} alt="Share" />
+            <div className="image-caption">SHARE</div>
+          </div>
+          <div className="image-card protect">
+            <img src={protectImage} alt="Protect" />
+            <div className="image-caption">PROTECT</div>
+          </div>
+          <div className="image-card engage">
+            <img src={engageImage} alt="Engage" />
+            <div className="image-caption">ENGAGE</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
