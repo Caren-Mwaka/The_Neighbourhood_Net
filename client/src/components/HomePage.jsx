fa53a50cd498c56196c6c3080027ed48391b8f8f
Tatiana Mathia <mathiatatiana@gmail.com>
2024-08-11 14:07:10 +0300

    homepage changes

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import './HomePage.css'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUsers, faShieldAlt, faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
-import photo1 from '../assets/events.jpg';
+import photo1 from '../assets/streetfest.jpg';
 import photo2 from '../assets/middlephoto.jpg';
 import photo3 from '../assets/teamphoto2.jpg';
+import photo4 from '../assets/barbequeue.jpeg';
+import photo5 from '../assets/sportsfan.jpeg';
 
 const Card = ({ imageSrc }) => (
   <div className="card">
@@ -20,6 +22,8 @@ const HomePage = () => {
           <Card imageSrc={photo1} />
           <Card imageSrc={photo2} />
           <Card imageSrc={photo3} />
+          <Card imageSrc={photo4} />
+          <Card imageSrc={photo5} />
         </div>
       </div>
       <div className="bottom-frames">
